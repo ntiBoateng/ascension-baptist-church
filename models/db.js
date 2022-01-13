c0dc2248c770775e623be3d367d84853ab4bb24b
ntiBoateng <gideon.boateng@amalitech.org>
2022-01-12 23:29:06 -0800

    changed from localhost to cloud cluster

--- models/db.js
+++ models/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/EmployeeDB', {useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+
+mongoose.connect('mongodb+srv://bgnti:Aladdin200@cluster0.vrp2h.mongodb.net/EmployeeDB', {useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
 
 require('./employee.model');
+
+//mongodb://127.0.0.1:27017/EmployeeDB'
